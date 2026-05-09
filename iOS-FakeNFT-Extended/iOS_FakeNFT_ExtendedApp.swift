dbc9306d432f9b80030f9f17ea442038f22b2768
Anton Silenin <anton-silenin@yandex.ru>
2026-05-09 15:36:38 +0300

    feat: add splash screen with light/dark variants

--- iOS-FakeNFT-Extended/iOS_FakeNFT_ExtendedApp.swift
+++ iOS-FakeNFT-Extended/iOS_FakeNFT_ExtendedApp.swift
@@ -6,6 +6,9 @@ struct iOS_FakeNFT_ExtendedApp: App {
     @State private var router = Router()
     @State private var services = ServicesAssembly(networkClient: DefaultNetworkClient())
 
+    init() {
+    }
+    
     var body: some Scene {
         WindowGroup {
             TabBarView()
