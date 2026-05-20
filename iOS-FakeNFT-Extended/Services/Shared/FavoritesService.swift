a15aad75ec48c2c22d0af4ee8d035356b39d055e
m.kozhukhov <neogenka@gmail.com>
2026-05-20 14:16:34 +0300

    fix: duplicated file deleted from target

--- iOS-FakeNFT-Extended/Services/Shared/FavoritesService.swift
+++ iOS-FakeNFT-Extended/Services/Shared/FavoritesService.swift
@@ -8,20 +8,21 @@
 import Foundation
 
 actor FavoritesService: FavoritesServiceProtocol {
-
+    
     private let networkClient: NetworkClient
-
+    
     init(networkClient: NetworkClient) {
         self.networkClient = networkClient
     }
-
+    
     func loadFavorites() async throws -> Set<String> {
         let profile: CatalogProfileDto = try await networkClient.send(request: ProfileGetRequest())
         return Set(profile.likes)
     }
-
+    
     func setFavorites(_ ids: Set<String>) async throws {
         let request = ProfileSetLikesRequest(likes: Array(ids.sorted()))
         let profile: CatalogProfileDto = try await networkClient.send(request: request)
         _ = profile
     }
+}
