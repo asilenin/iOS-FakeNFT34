438782b0774c43baac4e26f1385cc1e4c8def0af
Anton Silenin <anton-silenin@yandex.ru>
2026-05-25 13:36:16 +0300

    fix(statistics): fix collection grid overlap (cell height 192, drop price label)

--- iOS-FakeNFT-Extended/UI/Statistics/UserCollection/Views/Components/StatisticsNftGridCellView.swift
+++ iOS-FakeNFT-Extended/UI/Statistics/UserCollection/Views/Components/StatisticsNftGridCellView.swift
@@ -22,7 +22,7 @@ struct StatisticsNftGridCellView: View {
             imageWithFavoriteButton
             infoBlock
         }
-        .frame(width: 108, height: 172)
+        .frame(width: 108, height: 192)
         .contentShape(Rectangle())
         .onTapGesture(perform: actions.onCellTap)
     }
@@ -90,14 +90,10 @@ struct StatisticsNftGridCellView: View {
     }
 
     private var priceBlock: some View {
-        VStack(alignment: .leading, spacing: 2) {
-            Text("Statistics.nft.priceLabel")
-                .font(.medium10)
-                .foregroundStyle(Color.ypGrayUniversal)
-            Text(priceString)
-                .font(.medium10)
-                .foregroundStyle(Color.ypBlack)
-        }
+        Text(priceString)
+            .font(.medium10)
+            .foregroundStyle(Color.ypBlack)
+            .lineLimit(1)
     }
 
     private var priceString: String {
